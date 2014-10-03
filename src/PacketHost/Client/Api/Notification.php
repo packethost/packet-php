06083ffbf892da84f0ebf053cb160deecf8e5313
mustela <ejankowski@gmail.com>
2014-10-03 18:27:51 -0300

    New parameter added to the notification class

--- src/PacketHost/Client/Api/Notification.php
+++ src/PacketHost/Client/Api/Notification.php
@@ -3,7 +3,7 @@
 class Notification extends BaseApi {
 
     public function __construct( \PacketHost\Client\Adapter\AdapterInterface $adapter ){
-        parent::__construct( $adapter, 'notifications');
+        parent::__construct( $adapter, 'notifications', \PacketHost\Client\Domain\Notification::class);
     }
 
 }
