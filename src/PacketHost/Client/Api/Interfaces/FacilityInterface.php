22127cfeead2cee9ffb9953b2192250766dae8ea
Lucas Perez <lucasmpb@gmail.com>
2014-12-19 13:45:10 -0300

    Remove private methods from Facility Api

--- src/PacketHost/Client/Api/Interfaces/FacilityInterface.php
+++ src/PacketHost/Client/Api/Interfaces/FacilityInterface.php
@@ -3,12 +3,4 @@
 interface FacilityInterface {
     
     public function getAll( $options = []);
-    
-    public function get( $id, $options = []);
-
-    public function create( $data, $options = []);
-
-    public function delete( $id, $options = [] );
-
-    public function update( $id, $data, $options = [] );
 }
