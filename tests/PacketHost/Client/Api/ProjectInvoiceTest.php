ac586d8944703c848f592009e2b4b1979b4b9530
Lucas Perez <lucasmpb@gmail.com>
2015-01-02 13:00:24 -0300

    Improve project invoice test. mock adapters.

--- tests/PacketHost/Client/Api/ProjectInvoiceTest.php
+++ tests/PacketHost/Client/Api/ProjectInvoiceTest.php
@@ -3,30 +3,27 @@
  class ProjectInvoiceTest extends \PHPUnit_Framework_TestCase{
 
     private $api;
+    private $adapterMock;
 
     public function __construct(){
     
-        $this->api = new \PacketHost\Client\Api\ProjectInvoice( new \PacketHost\Client\Adapter\GuzzleAdapter( new \PacketHost\Client\Adapter\Configuration\JsonConfiguration( $this->getFileConfiguration()) ) );
-    }
+        $this->adapterMock = $this->getMockBuilder('\PacketHost\Client\Adapter\AdapterInterface')
+            ->disableOriginalConstructor()
+            ->getMock();
 
-    private function getFileConfiguration(){
-        $path = str_replace('PacketHost/Client/Api','',__DIR__)."config.json";
-        return $path;
+        $this->adapterMock->method('get')->willReturn((object)array("invoices"=>[]));
+
+        $this->api = new \PacketHost\Client\Api\ProjectInvoice( $this->adapterMock );
     }
 
     public function testGetAllSuccess()
     {
+        $this->adapterMock->expects($this->once())
+                 ->method('get')
+                 ->with($this->equalTo('projects/c3de2262-7bb6-450b-840e-cf62f66e9bf0/invoices/'));
+
         $response = $this->api->getAll('c3de2262-7bb6-450b-840e-cf62f66e9bf0');
 
         $this->assertEquals([], $response);
-    }
-
-    /**
-     * @expectedException \PacketHost\Client\Exceptions\ResponseExceptions\ClientResponseException
-     */
-    public function testGetAllNotFound()
-    {
-        
-        $response = $this->api->getAll('not-found');
     }
 }
