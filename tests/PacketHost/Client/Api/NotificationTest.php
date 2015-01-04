4003ed8b955a769a4616cb729d54500bf99ecec2
Lucas Perez <lucasmpb@gmail.com>
2015-01-04 11:45:13 -0300

    Test notifications api

---
+++ tests/PacketHost/Client/Api/NotificationTest.php
@@ -0,0 +1,52 @@
+<?php namespace Test\PacketHost\Client\Api;
+
+class NotificationTest extends BaseApiTest{
+
+    public function __construct(){
+
+        parent::__construct(\PacketHost\Client\Api\Notification::class);
+
+    }
+
+    public function testGetAll()
+    {
+        $this->mock->shouldReceive('get')
+            ->with('notifications/', array())
+            ->andReturn((object)array("notifications"=>[]))
+            ->once();
+        
+        $response = $this->api->getAll();
+
+        $this->assertEquals([], $response);
+    }
+
+    public function testGet()
+    {
+        $this->mock->shouldReceive('get')
+            ->with('notifications/860d6cc7-e42a-4977-83ea-8af1208a3613', array())
+            ->andReturn(array())
+            ->once();
+
+        $response = $this->api->get('860d6cc7-e42a-4977-83ea-8af1208a3613');
+
+        $this->assertEquals(new \PacketHost\Client\Domain\Notification(array()), $response);
+    }
+
+    public function testUpdate()
+    {
+        $notification = array(
+            'id' => '860d6cc7-e42a-4977-83ea-8af1208a3613',
+            'body' => 'test'
+        );
+
+        $this->mock->shouldReceive('patch')
+            ->with('notifications/860d6cc7-e42a-4977-83ea-8af1208a3613', $notification, array())
+            ->andReturn($notification)
+            ->once();
+
+        $response = $this->api->update('860d6cc7-e42a-4977-83ea-8af1208a3613', $notification);
+
+        $this->assertEquals(new \PacketHost\Client\Domain\Notification($notification), $response);
+    }
+
+}
