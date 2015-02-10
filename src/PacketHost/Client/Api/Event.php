fd29d0cd1d8905630069f612e446f2a39a332263
Lucas Perez <lucasmpb@gmail.com>
2015-02-10 08:35:44 -0300

    Add last login fields to User Model

--- src/PacketHost/Client/Api/Event.php
+++ src/PacketHost/Client/Api/Event.php
@@ -6,13 +6,13 @@ class Event extends BaseApi implements \PacketHost\Client\Api\Interfaces\EventIn
         parent::__construct( $adapter, 'events/:id', \PacketHost\Client\Domain\Event::class,'events');
     }
 
-    public function getAll( $options = ""){
+    public function getAll( $options = []){
 
         return $this->getEntities( $this->getParams(), $options);
     }
 
 
-    public function get( $id, $options = ""){
+    public function get( $id, $options = []){
 
         return $this->getEntity( $this->getParams( $id ), $options );
     }
--- src/PacketHost/Client/Domain/User.php
+++ src/PacketHost/Client/Domain/User.php
@@ -85,4 +85,8 @@ class User extends BaseDomain{
 
     public $projects;
 
+    public $lastLoginAt;
+
+    public $lastLoginFrom;
+
 }
