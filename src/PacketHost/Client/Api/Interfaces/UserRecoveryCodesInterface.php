2594000d118d77c47ffae6520f1c06a7798cede4
Emiliano Jankowski <ejankowski@gmail.com>
2015-05-06 16:24:04 +0200

    Revert "Add recovery codes domain"

--- src/PacketHost/Client/Api/Interfaces/UserRecoveryCodesInterface.php
+++
@@ -1,8 +0,0 @@
-<?php namespace PacketHost\Client\Api\Interfaces;
-
-interface UserRecoveryCodesInterface
-{
-    public function getAll($options = []);
-
-    public function create($options = []);
-}
