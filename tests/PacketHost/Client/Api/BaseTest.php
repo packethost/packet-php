6538c656943982707dda3bb55f3e1ce7a1deec1c
mustela <ejankowski@gmail.com>
2015-05-08 21:58:44 +0200

    Cleaning

--- tests/PacketHost/Client/Api/BaseTest.php
+++ tests/PacketHost/Client/Api/BaseTest.php
@@ -12,10 +12,32 @@ abstract class BaseTest extends \PHPUnit_Framework_TestCase
         $this->mock = \Mockery::mock('\PacketHost\Client\Adapter\AdapterInterface');
 
         $this->api = new $apiClass( $this->mock);
+
     }
 
     public function tearDown()
     {
         \Mockery::close();
+    }
+
+    protected function toCollection($type, $data){
+        $result = array();
+        foreach ($data as $key => $value)
+        {
+            $result[$key] = new $type($value);
+        }
+        return $result;
+    }
+
+    protected function toArray($data){
+        if (is_array($data) || is_object($data))
+        {
+            $result = array();
+            foreach ($data as $key => $value)
+            {
+                $result[$key] = $this->toArray($value);
+            }
+            return $result;
+        }
     }
 }
